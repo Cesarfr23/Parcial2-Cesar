4e9c4ee83925a516c3cc98b3b5325b5e25c25005
Cesarfr23 <cesarfernandez860@gmail.com>
2016-07-11 19:35:25 +0100

    Agregando Conexion y Capas

---
+++ Parcial2-Cesar/DALL/EmpleadosDb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALL
+{
+    public static class EmpleadosDb : DbContext
+    {
+        
+    }
+}
